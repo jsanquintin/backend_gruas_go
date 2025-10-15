db853470890a394412715694d84f3d5022e86919
Juan Sanquintin <jsanquintin@hotmail.com>
2025-10-15 12:10:22 -0400

    fix(main): corrección de ruta y soporte para ejecución directa con CMD ["python", "app/main.py"]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import os
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.api.routes import auth, user, service
